c77a20945c249373ad20fb0c3870d0379027bd67
Istvan Gellai <istvan.gellai@gmail.com>
2024-05-19 17:34:28 +0200

    Added external preamble processing and chapter merging

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,31 +1,57 @@
-macro(add_book_target MARKDOWN_FILE PDF_FILE)
+macro(add_book_target MARKDOWN_FILE PREAMBLE_FILE SUFFIX)
     # Get the directory of the markdown file relative to the source directory
     get_filename_component(MARKDOWN_DIR ${MARKDOWN_FILE} DIRECTORY)
 
     # Get the relative path from the source directory to the markdown file directory
-    file(RELATIVE_PATH REL_DIR ${CMAKE_SOURCE_DIR} ${MARKDOWN_DIR})
+    file(RELATIVE_PATH REL_DIR ${CMAKE_SOURCE_DIR}/sources/ ${MARKDOWN_DIR})
 
     # Construct the output directory based on the relative path
-    set(OUTPUT_DIR ${CMAKE_SOURCE_DIR}/output/${REL_DIR})
-
-    # Ensure the output directory exists
+    set(OUTPUT_DIR ${CMAKE_INSTALL_BINARY_DIR}/${REL_DIR})
     file(MAKE_DIRECTORY ${OUTPUT_DIR})
 
-    # Get the filename without the directory
-    get_filename_component(FILE_NAME ${PDF_FILE} NAME)
+    # Get the filename without the directory and add the suffix
+    get_filename_component(FILE_NAME ${MARKDOWN_FILE} NAME_WE)
 
-    # Construct the full path for the PDF file in the output directory
-    set(OUTPUT_PDF ${OUTPUT_DIR}/${FILE_NAME})
+    set(OUTPUT_FILE ${FILE_NAME}_${SUFFIX})
+    set(OUTPUT_FILE_PATH ${OUTPUT_DIR}/${FILE_NAME}_${SUFFIX})
 
     # Add custom target to generate PDF
     add_custom_target(
-            ${PDF_FILE}
+            ${OUTPUT_FILE}
             ALL
-            COMMAND pandoc ${MARKDOWN_FILE} -o ${OUTPUT_PDF}
+            COMMAND pandoc ${MARKDOWN_FILE} -o ${OUTPUT_FILE_PATH} --metadata-file=${PREAMBLE_FILE}
             DEPENDS ${MARKDOWN_FILE}
-            COMMENT "Generating PDF from Markdown using Pandoc"
+            COMMENT "Generating PDF from Markdown using Pandoc with preamble ${PREAMBLE_FILE}"
             WORKING_DIRECTORY ${MARKDOWN_DIR}
     )
 
-    message(STATUS "Generating PDF from ${MARKDOWN_FILE} to ${OUTPUT_PDF}")
+    message(STATUS "Generating FILE to ${OUTPUT_FILE_PATH}")
+endmacro()
+
+
+macro(add_combined_book_target TARGET_NAME PREAMBLE_FILE SUFFIX MARKDOWN_FILES)
+    # Create a space-separated list of Markdown files
+    get_filename_component(MARKDOWN_DIR ${PREAMBLE_FILE} DIRECTORY)
+    file(RELATIVE_PATH REL_DIR ${CMAKE_SOURCE_DIR}/sources/ ${MARKDOWN_DIR})
+
+    set(MARKDOWN_FILES_LIST "")
+    foreach(MARKDOWN_FILE ${MARKDOWN_FILES})
+        set(MARKDOWN_FILES_LIST "${MARKDOWN_FILES_LIST} ${MARKDOWN_FILE}")
+    endforeach()
+
+    # Set the output PDF file name
+    set(OUTPUT_DIR ${CMAKE_INSTALL_BINARY_DIR}/${REL_DIR})
+    set(OUTPUT_PDF ${OUTPUT_DIR}/${TARGET_NAME}_${SUFFIX}.pdf)
+
+    # Add custom target to generate combined PDF
+    add_custom_target(
+            ${TARGET_NAME}_${SUFFIX}
+            ALL
+            COMMAND pandoc ${MARKDOWN_FILES} -o ${OUTPUT_PDF} --metadata-file=${PREAMBLE_FILE}
+            DEPENDS ${MARKDOWN_FILES}
+            COMMENT "Generating combined PDF from Markdown using Pandoc with preamble ${PREAMBLE_FILE}"
+            WORKING_DIRECTORY ${MARKDOWN_DIR}
+    )
+
+    message(STATUS "Generating combined PDF ${OUTPUT_PDF}")
 endmacro()
